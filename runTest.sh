57948c2074e31db845014c413e73e7bf54be3461
Neena Pandey <neenapandey7@gmail.com>
2023-09-08 12:44:44 +1000

    .

--- runTest.sh
+++ runTest.sh
@@ -1,1 +1,1 @@
-newman run postman_collections/Employee_collection.postman_collection.json -d data/EMP_DATA.csv -e env/qa.EMP_postman_environment.json
+newman run postman_collections/Employee_collection.postman_collection.json -d data/EMP_DATA.csv -e env/qa.EMP_postman_environment.json -r cli,htmlextra --reporter-htmlextra-export "newman/newman_result.html"
